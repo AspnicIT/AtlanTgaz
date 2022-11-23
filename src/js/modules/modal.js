3a01b75d11773d1eb57e00ead0e5a76ea062fbdf
Sergei <111318456+AspnicIT@users.noreply.github.com>
2022-11-24 04:53:45 +0500

    Add files via upload

---
+++ src/js/modules/modal.js
@@ -0,0 +1,62 @@
+
+function modal(
+    selButtons,
+    selCloser,
+    selModal,
+    selForm,
+    selFocusInput,
+    selActivClass
+) {
+    const buttons = document.querySelectorAll(selButtons),
+        closer = document.querySelector(selCloser),
+        modal = document.querySelector(selModal),
+        formModal = document.querySelector(selForm),
+        focusInput = document.querySelector(selFocusInput),
+        activClass = selActivClass;
+
+    function modalOpen(block, form, cssClass) { 
+
+        block.classList.remove(cssClass);
+        document.body.style.overflow = 'hidden';
+        focusInput.focus();
+        form.reset();
+
+    }
+
+    function modalClose(block, cssClass) {
+        block.classList.add(cssClass);
+        document.body.style.overflow = '';
+    }
+
+    buttons.forEach((item) => {
+        item.addEventListener('click', () => {
+            modalOpen(modal, formModal, activClass);
+        });
+    });
+    buttons.forEach((item) => {
+        item.addEventListener('toushstart', () => {
+            modalOpen(modal, formModal, activClass);
+        });
+    });
+
+    closer.addEventListener('click', () => {
+        modalClose(modal, activClass);
+    });
+    closer.addEventListener('touchstart', () => {
+        modalClose(modal, activClass);
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modalClose(modal, activClass);
+        }
+    });
+
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            modalClose(modal, activClass);
+        }
+    });
+    // buttons.forEach(item => item.addEventListener('click', ()=> modal.classList.remove('hide')));
+}
+export default modal;
